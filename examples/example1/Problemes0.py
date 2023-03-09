f7f5cb0773b9f998154dcc834f8d08295dcc4063
Jordi Saludes <jordi.saludes@upc.edu>
2023-03-09 20:42:30 +0100

    Added a strategy for fractional complex numbers.

--- examples/example1/Problemes0.py
+++ examples/example1/Problemes0.py
@@ -1,24 +1,40 @@
-from hypothesis.strategies import integers, complex_numbers
+from hypothesis.strategies import integers, complex_numbers, composite
 from exams import LaTeXExamExam
 
 ex = LaTeXExamExam()
 
 complex = complex_numbers(min_magnitude=1, max_magnitude=2)
 smallint = integers(min_value=2,max_value=6)
-    
+
+
+@composite
+def sympy_complex(draw,min_magnitude=1, max_magnitude=2, tolerance=0.01):
+    from hypothesis.strategies import fractions
+    from hypothesis import assume
+    from sympy import I, nsimplify
+    z = draw(complex_numbers(min_magnitude=min_magnitude, max_magnitude=max_magnitude))
+    z_ = nsimplify(z, rational=True, tolerance=tolerance)
+    assume(z_.as_real_imag()[1] != 0)
+    return z_
+
+
 
 
 @ex.problem
 def potencia(draw):
     """Calculeu la potència {{EXPONENT}} de {{COMPLEX}}"""
-    z = draw(complex)
+    scx = sympy_complex(min_magnitude=1, max_magnitude=2, tolerance=0.1)
+    z = draw(scx)
+    # z = draw(complex)
     n = draw(smallint)
-    return dict(COMPLEX=z, EXPONENT=n)
+    zn = z**n
+    return dict(COMPLEX=z, EXPONENT=n, SOLUCIO=zn)
 
 @ex.problem
 def arrel(draw):
     """Calculeu $\sqrt[{{EXPONENT}}]{ {{COMPLEX}} }$"""
-    return dict(
-        COMPLEX=draw(complex),
-        EXPONENT=draw(smallint))
+    z = draw(complex)
+    n = draw(smallint)
+    zn = z**(1/n)
+    return dict(COMPLEX=z, EXPONENT=n, SOLUCIO=zn)
 
